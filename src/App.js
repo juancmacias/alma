1e89e19d20e7ebcf60463104a0af4cf6f75aaebe
juan carlos macias <juancmaciassalvador@gmail.com>
2023-08-29 09:54:06 +0200

    Varias cosas

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@
 import Frame from './Componentes/Frame';
 import Inicial from './Componentes/Inicial';
 import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
-
   return (
     <div>
-
         <Routes>
           <Route exact path="/" element={<Inicial/>} />
           <Route path="/hubs" element={<Frame/>} />
         </Routes>
-
-
     </div>
-
   );
 }
 
