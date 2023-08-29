02a36005736a7665d01ebca40530d7f2c238fd45
juan carlos macias <juancmaciassalvador@gmail.com>
2023-08-29 09:30:46 +0200

    menú

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import Formulario from './Componentes/Formulario';
+
 import Frame from './Componentes/Frame';
 import Inicial from './Componentes/Inicial';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 
 
 function App() {
 
   return (
     <div>
-      <Routes>
-        <Route exact path="/" element={<Inicial/>} />
-        <Route exact path="/contacto" element={<Formulario/>} />
-        <Route path="/hubs" element={<Frame/>} />
-      </Routes>
+
+        <Routes>
+          <Route exact path="/" element={<Inicial/>} />
+          <Route path="/hubs" element={<Frame/>} />
+        </Routes>
+
+
     </div>
 
   );
--- src/Componentes/Formulario.jsx
+++ src/Componentes/Formulario.jsx
@@ -2,7 +2,7 @@ function Formulario() {
 
     return (
       <div>
-        contacto
+        contacto/formulario
       </div>
   
     );
