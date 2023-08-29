44cea4fec55ac552a2f5999bb443da1e77d19852
juan carlos macias <juancmaciassalvador@gmail.com>
2023-08-29 14:25:24 +0200

    Varias escenas

---
+++ src/Componentes/SesionCinco.jsx
@@ -0,0 +1,14 @@
+import {Entity, Scene} from 'aframe-react';
+function SesionCinco() {
+    return (
+        <Scene physics="debug: false" canvas="" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" auto-enter-vr="">
+        
+        {/* CAMARA */}
+        <Entity camera kinematic-body="radius:2"  look-controls universal-controls position="0 1.6 0">
+            <Entity cursor position="0 0 -0.5" geometry="primitive:ring;radiusInner:0.01;radiusOuter:0.016" material="opacity:0.5;shader:flat;transparent:false;color:blue" scale="0.8 0.8 0.8" raycaster ></Entity>
+        </Entity>
+        </Scene>
+    );
+}
+
+export default SesionCinco;
