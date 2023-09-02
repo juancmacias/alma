506455aaeb2e7285e34e557dc508e99b1905f4e6
juan carlos macias <juancmaciassalvador@gmail.com>
2023-09-02 08:29:00 +0200

    Optimización, Sonido en el indice, posicionamiento

--- src/Componentes/Frame.jsx
+++ src/Componentes/Frame.jsx
@@ -4,6 +4,7 @@ import {Entity, Scene} from 'aframe-react';
 import React, {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import useSound from 'use-sound';
+import forest from '../sound/jardin.mp3';
 
 /* precargar los objetos en 3d */
 import bamboo from '../obj/plantas/bamboo.glb'
@@ -19,11 +20,38 @@ import nomo from '../obj/construccion/mesh_garden_gnome.glb'
 import tori from '../obj/construccion/japanese_tori_gate.glb'
 import nubecumulo from '../obj/nubes/CumulusClouds.glb'
 import portalValores from '../obj/construccion/gate_wood.glb'
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
 
 function Frame() {
-  
-  const cargarObjetos = (tipo1,tipo2, cantidad, p, max, min, x,y,z, escala) =>{
+const audio = new Audio(forest);
+audio.loop = true;
+//const [play, setPlay] = useState('');
+const [sonido, setSonido] = useState(localStorage.getItem('sonido'));
+const start = ()  => {
+   /* carga de sonido */
+   
+  if(sonido === "on"){
+    audio.play();
+    //setPlay("src:#forest; autoplay:true; loop:true; volume:0.05")
+    console.log("sonido on" + localStorage.getItem('sonido')+ " ll")
+    
+  }else{
+    //setPlay('');
+    console.log("sonido off "+ localStorage.getItem('sonido')+ " ll")
+  }
+  console.log(document.querySelector('a-assets').fileLoader);
+
+}
+
+useEffect(()=> {
+  start();
+})
+const keyPress = (e)=>{
+    console.log("pulsas tecla "+e.keyCode)
+}
+
+  const cargarObjetos = (tipo1,tipo2, cantidad, p, max, min, x,y,z, escala) => {
     let objetos = []
     for(let i = 1; i <= cantidad; i++){     
       let rot = 180/i+5
@@ -31,24 +59,30 @@ function Frame() {
       let XX = x + Math.random()
       let ZZ = 1 + Math.floor(Math.random() * (max - min + 1) + min);
 
-      objetos.push(<Entity static-body id={`arbol_5${i}`} gltf-model={tipo1} position={`${XX} ${y} ${ZZ}`} rotation={`0 ${rot}) 0`} scale={`${scala} ${scala} ${scala}`}></Entity>)
+      objetos.push(<Entity static-body="true" id={`arbol_5${i}`} gltf-model={tipo1} position={`${XX} ${y} ${ZZ}`} rotation={`0 ${rot}) 0`} scale={`${scala} ${scala} ${scala}`}></Entity>)
       objetos.push(<a-gltf-model id={`hoja_5${i}`}  src={tipo2} position={`${XX} ${y} ${ZZ}`} rotation={`0 ${rot}) 0`} scale={`${scala} ${scala} ${scala}`}></a-gltf-model>)
     }
     return(objetos)
 
  }
  const portalSeleccion = (ir) => {
-  console.log("Hecho " + ir)
+  var pos = document.querySelector('#camera').getAttribute('position');
+  console.log("Hecho " + pos.x)
+  console.log("Hecho " + pos.z)
+  console.log("Hecho " + pos.y)
+  var position = pos.x+ ' '+ pos.y+ ' '+pos.z 
+  localStorage.setItem("position", position);
+
   window.location.href=ir;
  }
  
   const cargarCamara = (
-    <Entity camera  kinematic-body="radius:1" universal-controls position="0 1.6 19.5" rotation="16.6 0 0">
+    <Entity id="camera" camera kinematic-body="radius:1" universal-controls look-controls position={localStorage.getItem('position') === null ? "0 1.6 19.5" : localStorage.getItem('position')} rotation="16.6 0 0">
       <Entity  cursor position="0 0 -0.5" geometry="primitive:ring;radiusInner:0.01;radiusOuter:0.016" material="opacity:0.5;shader:flat;transparent:false;color:blue" scale="0.5 0.5 0.5"></Entity>
     </Entity>
   );
     return (
-      <Scene physics="debug: true" canvas="" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="true" auto-enter-vr="">
+      <Scene  onKeyDown={keyPress} physics="debug: false;friction:0.1;restitution:0.5" canvas="" inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="true" auto-enter-vr="">
         {/*
         <a-entity id="mano_left" hand-controls="left"></a-entity>
         <a-entity id="mano_right" hand-controls="right"></a-entity>
@@ -57,15 +91,16 @@ function Frame() {
 
         {/* recursos xrextras-loading */}
         <a-assets>
-          <img id="imagen-pared" src={require('../resources/piedra_muro.jpg')} alt='' />
+
+          <img id="imagen-pared" src={require('../resources/piedra_muro_1.jpg')} alt='' />
           <img id="cesped" src={require('../resources/cesped.jpg')} alt='' />
           <img id="sky" src={require('../resources/cielo-azul.jpg')} alt='' />
-          <img id="camino_piedra" src={require('../resources/piedra_suelo.jpg')} alt='' />
+          <img id="camino_piedra" src={require('../resources/piedra_suelo_1.jpg')} alt='' />
 
         </a-assets>
-        
+
         <Entity geometry="height: 5;segmentsHeight:30;segmentsRadial:30; primitive:cylinder;" color="black" static-body="shape: cylinder" position="1.9 0 0" rotation="0 -11 0" scale="4.5 1 6.3"/>
-        <Entity id="muro_1" position="-15 0 0" rotation="0 0 0">
+        <Entity  id="muro_1" position="-15 0 0" rotation="0 0 0">
           {
             /* 
              Arboles del perimetro
@@ -102,25 +137,25 @@ function Frame() {
           <a-box id="wall_11" color="grey" width="30" height="3" depth="1" position="0 0 0" material="repeat:19 5;color:withe;metalness:0.2;roughness:0.1;src:#imagen-pared" static-body rotation="0 90 0" ></a-box>
         </Entity>
 
-        <Entity id="muro_2" position="15 0 0" rotation="0 0 0">
+        <Entity onLoaded={() => { console.log('carga.....'); }} id="muro_2" position="15 0 0" rotation="0 0 0">
           {/* Arboles del perimetro  "dynamic-body="mass: 5"*/}
-          <a-entity static-body id="arbol_21" gltf-model={arbol} position="-0.67 0 1" rotation="0 0 0" scale="1.0 1.0 1.0"></a-entity>
+          <a-entity static-body="true" id="arbol_21" gltf-model={arbol} position="-0.67 0 1" rotation="0 0 0" scale="1.0 1.0 1.0"></a-entity>
           <a-entity id="hojas_21" gltf-model={hojas} position="-0.67 0 1" rotation="0 0 0" scale="1.0 1.0 1.0"></a-entity>
-          <a-entity static-body id="arbol_22" gltf-model={arbol} position="-0.7 0 4.6" rotation="0 80 0" scale="1.0 1.0 1.0"></a-entity>
+          <a-entity static-body="true" id="arbol_22" gltf-model={arbol} position="-0.7 0 4.6" rotation="0 80 0" scale="1.0 1.0 1.0"></a-entity>
           <a-entity id="hojas_22" gltf-model={hojas} position="-0.7 0 4.6" rotation="0 80 0" scale="1.0 1.0 1.0"></a-entity>
-          <a-entity static-body id="arbol_23" gltf-model={arbol} position="-0.9 -0.5 8" rotation="0 119 0" scale="1.1 1.1 1.1"></a-entity>
+          <a-entity static-body="true" id="arbol_23" gltf-model={arbol} position="-0.9 -0.5 8" rotation="0 119 0" scale="1.1 1.1 1.1"></a-entity>
           <a-entity id="hojas_23" gltf-model={hojas} position="-0.9 -0.5 8" rotation="0 119 0" scale="1.1 1.1 1.1"></a-entity>
-          <a-entity static-body id="arbol_24" gltf-model={arbol} position="-0.66 -0.1 -6.58" rotation="0 39 0" scale="1.2 1.2 1.2"></a-entity>
+          <a-entity static-body="true" id="arbol_24" gltf-model={arbol} position="-0.66 -0.1 -6.58" rotation="0 39 0" scale="1.2 1.2 1.2"></a-entity>
           <a-entity id="hojas_24" gltf-model={hojas} position="-0.66 -0.1 -6.58" rotation="0 39 0" scale="1.2 1.2 1.2"></a-entity>
-          <a-entity static-body id="arbol_25" gltf-model={arbol} position="-0.6 -0.0 10.4" rotation="0 119 0" scale="1.4 1.4 1.4 "></a-entity>
+          <a-entity static-body="true" id="arbol_25" gltf-model={arbol} position="-0.6 -0.0 10.4" rotation="0 119 0" scale="1.4 1.4 1.4 "></a-entity>
           <a-entity id="hojas_25" gltf-model={hojas} position="-0.6 -0.0 10.4" rotation="0 119 0" scale="1.4 1.4 1.4 "></a-entity>
-          <a-entity static-body id="arbol_26" gltf-model={arbol} position="-0.5 -0.2 14.4" rotation="0 101 0" scale="1.7 1.7 1.7"></a-entity>
+          <a-entity static-body="true" id="arbol_26" gltf-model={arbol} position="-0.5 -0.2 14.4" rotation="0 101 0" scale="1.7 1.7 1.7"></a-entity>
           <a-entity id="hojas_26" gltf-model={hojas} position="-0.5 -0.2 14.4" rotation="0 101 0" scale="1.7 1.7 1.7"></a-entity>
-          <a-entity static-body id="arbol_27" gltf-model={arbol} position="-0.7 -0.5 -2.4" rotation="0 15 0" scale="1.6 1.6 1.6"></a-entity>
+          <a-entity static-body="true" id="arbol_27" gltf-model={arbol} position="-0.7 -0.5 -2.4" rotation="0 15 0" scale="1.6 1.6 1.6"></a-entity>
           <a-entity id="hojas_27" gltf-model={hojas} position="-0.7 -0.5 -2.4" rotation="0 15 0" scale="1.6 1.6 1.6"></a-entity>
-          <a-entity static-body id="arbol_28" gltf-model={arbol} position="-1 -0.05 -12.4" rotation="0 181 0" scale="1.2 1.2 1.2"></a-entity>
+          <a-entity static-body="true" id="arbol_28" gltf-model={arbol} position="-1 -0.05 -12.4" rotation="0 181 0" scale="1.2 1.2 1.2"></a-entity>
           <a-entity id="hojas_28" gltf-model={hojas} position="-1 -0.05 -12.4" rotation="0 181 0" scale="1.2 1.2 1.2"></a-entity>
-          <a-entity static-body id="arbol_29" gltf-model={arbol} position="-1.2 -0.05 -14.5" rotation="0 151 0" scale="1.2 1.2 1.2"></a-entity>
+          <a-entity static-body="true" id="arbol_29" gltf-model={arbol} position="-1.2 -0.05 -14.5" rotation="0 151 0" scale="1.2 1.2 1.2"></a-entity>
           <a-entity id="hojas_29" gltf-model={hojas} position="-1.2 -0.05 -14.5" rotation="0 151 0" scale="1.2 1.2 1.2"></a-entity>
 
 
@@ -141,23 +176,23 @@ function Frame() {
 
         <Entity id="muro_3" position="0 0 -15" rotation="0 90 0">
           {/* Arboles del perimetro */}
-          <a-entity static-body id="arbol_31" gltf-model={arbol} position="-0.67 0 1" rotation="0 0 0" scale="1 1 1"></a-entity>
+          <a-entity static-body="true" id="arbol_31" gltf-model={arbol} position="-0.67 0 1" rotation="0 0 0" scale="1 1 1"></a-entity>
           <a-entity id="hojas_31" gltf-model={hojas} position="-0.67 0 1" rotation="0 0 0" scale="1 1 1"></a-entity>
-          <a-entity static-body id="arbol_32" gltf-model={arbol} position="-0.7 0 4.6" rotation="0 80 0" scale="1 1 1"></a-entity>
+          <a-entity static-body="true" id="arbol_32" gltf-model={arbol} position="-0.7 0 4.6" rotation="0 80 0" scale="1 1 1"></a-entity>
           <a-entity id="hojas_32" gltf-model={hojas} position="-0.7 0 4.6" rotation="0 80 0" scale="1 1 1"></a-entity>
-          <a-entity static-body id="arbol_33" gltf-model={arbol} position="-0.9 -0.5 8" rotation="0 119 0" scale="1.1 1. 1.1"></a-entity>
+          <a-entity static-body="true" id="arbol_33" gltf-model={arbol} position="-0.9 -0.5 8" rotation="0 119 0" scale="1.1 1. 1.1"></a-entity>
           <a-entity id="hojas_33" gltf-model={hojas} position="-0.9 -0.5 8" rotation="0 119 0" scale="1.1 1. 1.1"></a-entity>
-          <a-entity static-body id="arbol_34" gltf-model={arbol} position="-0.66 -0.1 -6.58" rotation="0 39 0" scale="1.2 1.2 1.2"></a-entity>
+          <a-entity static-body="true" id="arbol_34" gltf-model={arbol} position="-0.66 -0.1 -6.58" rotation="0 39 0" scale="1.2 1.2 1.2"></a-entity>
           <a-entity id="hojas_34" gltf-model={hojas} position="-0.66 -0.1 -6.58" rotation="0 39 0" scale="1.2 1.2 1.2"></a-entity>
-          <a-entity static-body id="arbol_35" gltf-model={arbol} position="-0.6 -0.0 10.4" rotation="0 119 0" scale="1.4 1.4 1.4"></a-entity>
+          <a-entity static-body="true" id="arbol_35" gltf-model={arbol} position="-0.6 -0.0 10.4" rotation="0 119 0" scale="1.4 1.4 1.4"></a-entity>
           <a-entity id="hojas_35" gltf-model={hojas} position="-0.6 -0.0 10.4" rotation="0 119 0" scale="1.4 1.4 1.4"></a-entity>
-          <a-entity static-body id="arbol_36" gltf-model={arbol} position="-0.5 -0.2 14.4" rotation="0 101 0" scale="1.7 1.7 1.7"></a-entity>
+          <a-entity static-body="true" id="arbol_36" gltf-model={arbol} position="-0.5 -0.2 14.4" rotation="0 101 0" scale="1.7 1.7 1.7"></a-entity>
           <a-entity id="hojas_36" gltf-model={hojas} position="-0.5 -0.2 14.4" rotation="0 101 0" scale="1.7 1.7 1.7"></a-entity>
           <a-entity static-body id="arbol_37" gltf-model={arbol} position="-0.7 -0.5 -2.4" rotation="0 15 0" scale="1.6 1.6 1.6"></a-entity>
           <a-entity id="hojas_37" gltf-model={hojas} position="-0.7 -0.5 -2.4" rotation="0 15 0" scale="1.6 1.6 1.6"></a-entity>
-          <a-entity static-body id="arbol_38" gltf-model={arbol} position="-1 -0.05 -12.4" rotation="0 181 0" scale="1.2 1.2 1.2"></a-entity>
+          <a-entity static-body="true" id="arbol_38" gltf-model={arbol} position="-1 -0.05 -12.4" rotation="0 181 0" scale="1.2 1.2 1.2"></a-entity>
           <a-entity id="hojas_38" gltf-model={hojas} position="-1 -0.05 -12.4" rotation="0 181 0" scale="1.2 1.2 1.2"></a-entity>
-          <a-entity static-body id="arbol_39" gltf-model={arbol} position="-1.2 -0.05 -14.5" rotation="0 151 0" scale="1.2 1.2 1.2"></a-entity>
+          <a-entity static-body="true" id="arbol_39" gltf-model={arbol} position="-1.2 -0.05 -14.5" rotation="0 151 0" scale="1.2 1.2 1.2"></a-entity>
           <a-entity id="hojas_39" gltf-model={hojas} position="-1.2 -0.05 -14.5" rotation="0 151 0" scale="1.2 1.2 1.2"></a-entity>
 
           {/* bambu */}
@@ -219,15 +254,15 @@ function Frame() {
         {/* cosas varias */}
 
         <a-entity id="buda" gltf-model={buda} position="-0.5 -0.25 -14" rotation="0 29 0" scale="1 1 1"></a-entity>
-        <a-entity id="nomo_!" gltf-model={nomo} position="-14 0.01 -14" rotation="0 29 0" scale="1.2 1.2 1.2"></a-entity>
+        <a-entity id="nomo_1" gltf-model={nomo} position="-14 0.01 -14" rotation="0 29 0" scale="1.2 1.2 1.2"></a-entity>
         <a-entity id="nomo_2" gltf-model={nomo} position="14 0.01 14" rotation="0 -100 0" scale="1.2 1.2 1.2"></a-entity>
         <a-entity id="nomo_3" gltf-model={nomo} position="-14 0.01 14.5" rotation="0 100 0" scale="1.2 1.2 1.2"></a-entity>
         <a-entity id="nomo_4" gltf-model={nomo} position="14 0.01 -14" rotation="0 -29 0" scale="1.2 1.2 1.2"></a-entity>
 
         {/* ENTRADA */}
-        <a-box id="wall_seguro_1" width="30" height="3" depth="0.01" position="0 0 20" material="opacity:0.1" static-body rotation="0 180 0" ></a-box>
-        <a-box id="wall_seguro_2" width="10" height="3" depth="0.01" position="15 0 15" material="opacity:0.1" static-body rotation="0 90 0" ></a-box>
-        <a-box id="wall_seguro_3" width="10" height="3" depth="0.01" position="-15 0 -15" material="opacity:0.1" static-body rotation="0 -90 0" ></a-box>
+        <a-box id="wall_seguro_1" width="30" height="3" depth="0.01" position="0 0 20" material="opacity:0.1" static-body="true" rotation="0 180 0" ></a-box>
+        <a-box id="wall_seguro_2" width="10" height="3" depth="0.01" position="15 0 15" material="opacity:0.1" static-body="true" rotation="0 90 0" ></a-box>
+        <a-box id="wall_seguro_3" width="10" height="3" depth="0.01" position="-15 0 15" material="opacity:0.1" static-body="true" rotation="0 -90 0" ></a-box>
         <a-box id="wall_tory_1" color="grey" width="13.1" height="3" depth="0.5" position="9.26 0 15" material="repeat:19 5;color:withe;metalness:0.2;roughness:0.1;src:#imagen-pared" static-body rotation="0 180 0" ></a-box>
         <a-box id="wall_tory_2" color="grey" width="13.1" height="3" depth="0.5" position="-9.26 0 15" material="repeat:19 5;color:withe;metalness:0.2;roughness:0.1;src:#imagen-pared" static-body rotation="0 180 0" ></a-box>
         <a-entity id="tori" gltf-model={tori} position="0 0 15" rotation="0 0 0" scale="0.7 0.7 0.7"></a-entity>
@@ -265,8 +300,8 @@ function Frame() {
         <a-box width="2" height="3" depth="0.5" material="src:#camino_piedra" id="camino_31" position="-3.87 -0.2 -10" rotation="90 40 0"></a-box>
         <a-box width="2" height="3" depth="0.5" material="src:#camino_piedra" id="camino_32" position="-8.36 -0.2 -3" rotation="90 86.2 0"></a-box>
         {/* suelo */}
-        <a-plane id="plano_1" static-body position="0 0 0" rotation="-90 0 0" width="30" height="40" material="src: #cesped; repeat: 100 100" radius="10"></a-plane>
-        <a-plane id="plano_2" static-body position="0 -0.5 0" rotation="-90 0 0" width="300" height="400" color="green" radius="10"></a-plane>
+        <a-plane id="plano_1" static-body="true" position="0 0 0" rotation="-90 0 0" width="30" height="40" material="src: #cesped; repeat: 100 100" radius="10"></a-plane>
+        <a-plane id="plano_2"  position="0 -0.5 0" rotation="-90 0 0" width="300" height="400" color="green" radius="10"></a-plane>
         {/* cielo */}
         <a-sky id="cielo" color="#87CEEB" material="" geometry="" scale="-1.31 1 1" rotation="0 0 0"></a-sky>
         {/* nubes */}
